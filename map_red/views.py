621d3ce53f84dda016f01d90336615fe076c6bab
Dennis Mårtensson <dennismartensson@gmail.com>
2012-06-07 12:35:17 -0400

    fixt bug with all machins doing reduce, added jobs_out list to know it there are jobbs out in the network

--- map_red/views.py
+++ map_red/views.py
@@ -5,21 +5,26 @@ from django.shortcuts import render, redirect, get_object_or_404
 timesEach = [1] * 1000000
 timsTotal = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 map_jobs = [(x, timesEach) for x in timsTotal]
-#map_jobs = [1, 2 ,3, 4, 5]
 reduce_jobs = []
 result = None
 
+jobs_out = []
+
 def index(request):
-	#Can get different end results do to calculations on direrent host not done but map_jobs empty
+
 	if map_jobs:
 		function_to_run = "map"
 		job_content = map_jobs.pop()
+		jobs_out.append(1)
 		count = job_content[0]
-	elif reduce_jobs:
+	elif jobs_out and not map_jobs:
+		function_to_run = "wait"
+		job_content = "0"
+		count = "wait"
+	elif reduce_jobs and not jobs_out:
 		function_to_run = "reduce"
 		job_content = reduce_jobs;
 		count = job_content
-		print(job_content)
 	else:
 		return render(request, 'done.html', {"result": result})
 
@@ -30,9 +35,8 @@ def reduce(request):
 	reduceData = request.GET.get('count', None)
 
 	if reduceData:
-		print(reduceData)
+		jobs_out.pop()
 		reduce_jobs.append(int(float(reduceData)))
-		print(reduce_jobs)
 	return redirect('index')
 
 
@@ -40,6 +44,5 @@ def finalize(request):
 	finalizeData = request.GET.get('sum', None)
 
 	if finalizeData:
-		print(finalizeData)
 		result = finalizeData
 	return render(request, 'done.html', {"result": result})
